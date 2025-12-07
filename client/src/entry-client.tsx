8dce5fa33012729f6ea8804e589d132b61450854
TeaChoco <85914523+TeaChoco@users.noreply.github.com>
2025-12-07 12:35:29 +0700

    feat: Add client-side entry point with React StrictMode and BrowserRouter.

--- client/src/entry-client.tsx
+++ client/src/entry-client.tsx
@@ -2,11 +2,10 @@
 import "./index.css";
 import App from "./router/App";
 import { StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-hydrateRoot(
-    document.getElementById("root")!,
+createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter basename="/TeaChoco">
             <App />
